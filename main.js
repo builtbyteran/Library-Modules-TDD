8a972a53d78d1d3a2e44a7ad6f844ea9696a940a
Teran <teranarthur99@gmail.com>
2026-01-27 13:56:11 -0700

    Added functions

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 let Library = () => {
   return {
     addBook: function () {},
+    checkOutBook: function (book) {
+      if (!book['checkedOut']) {
+        book['checkedOut'] = true;
+        console.log(book['checkedOut']);
+      }
+    },
   };
 };
 
-let Book = () => {};
+let Book = () => {
+  return {
+    getAttribute: function (book, attribute) {
+      if (book.hasOwnProperty(attribute)) {
+        return book.attribute;
+      }
+    },
+    setAttribute: function () {},
+  };
+};
